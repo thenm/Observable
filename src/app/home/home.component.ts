022b36226c71929025685a3947cfb7b936fc8c53
thenm <nitinmaul.nitty@gmail.com>
2020-09-03 22:26:12 +0530

    Unsubscribing to the subscription

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,14 +7,23 @@ import { interval } from 'rxjs';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  // Unsubscribing to an subscription      
+  private firstObservable: Subscription;
   constructor() { }
 
   ngOnInit() {
     // interval returns an observable which emits a data stream after sepific time. 
-    interval(1000).subscribe(count => {
+    // Store the subscription which is returned by the subscribe. 
+    // this.firstObservable will store the subscription
+    this.firstObservable = interval(1000).subscribe(count => {
       console.log(count);
     });
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.firstObservable.unsubscribe();
   }
 
 }
